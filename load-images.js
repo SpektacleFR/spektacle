1a80e5b67aeeef92c5e526f6db0ab1af3b19427b
SpektacleFR <zacharygough07@gmail.com>
2023-08-02 11:31:21 -0400

    Thumbnail Implementation

--- load-images.js
+++ load-images.js
@@ -1,5 +1,23 @@
 var folder = 'img/render/';
-    
+
+function isString(variable) { 
+    return typeof (variable) === 'string'; 
+}
+
+function replaceLastOccurrenceInString(input, find, replaceWith) {
+    if (!isString(input) || !isString(find) || !isString(replaceWith)) {
+        // returns input on invalid arguments
+        return input;
+    }
+
+    const lastIndex = input.lastIndexOf(find);
+    if (lastIndex < 0) {
+        return input;
+    }
+
+    return input.substr(0, lastIndex) + replaceWith + input.substr(lastIndex + find.length);
+}
+
 $.ajax({
     url : folder,
     success: function (data) {
@@ -7,14 +25,14 @@ $.ajax({
             if (val.match(/\.(jpe?g|png|gif)$/)) { 
                 for (var i = 1; i <= 1; i++) {
                     $(`<a href="${val}" class="interactable image">
-                    <img src="${val}" class="render" alt="render">
+                    <img src="${replaceLastOccurrenceInString(val, '/', '/thumbnails/')}" class="render" alt="render">
                     </a>`).appendTo('#renders');
+                    console.log(replaceLastOccurrenceInString(val, '/', '/thumbnails/'))
                 }
             } 
         });
     }
 });
-
 
 /*<a href="img/render/deck.png" class="interactable image">
     <img src="img/render/deck.png" class="render" alt="render">
