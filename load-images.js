f84e71a5b892f7deb97a6e70cac5a3a3b72c8d96
SpektacleFR <zacharygough07@gmail.com>
2023-08-02 10:07:30 -0400

    Fixing the Dynamic Portfolio

--- load-images.js
+++ load-images.js
@@ -1,20 +1,27 @@
-var folder = "img/render/";
-    
-$.ajax({
-    url : folder,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if (val.match(/\.(jpe?g|png|gif)$/)) { 
-                for (var i = 1; i <= 1; i++) {
-                    $(`<a href="${val}" class="interactable image">
-                    <img src="${val}" class="render" alt="render">
-                    </a>`).appendTo('#renders');
-                }
-            } 
-        });
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+var client = new HttpClient();
+client.get('https://api.github.com/repos/SpektacleFR/spektacle/contents/img/render', function(response) {
+    const object = JSON.parse(response);
+    console.log(object);
+    for (var i of object) {
+        console.log(i.download_url)
+        document.getElementById('renders').innerHTML += `<a href="${i.download_url}" class="interactable image">
+            <img src="${i.download_url}" class="render" alt="render">
+            </a>`
     }
 });
-
 
 /*<a href="img/render/deck.png" class="interactable image">
     <img src="img/render/deck.png" class="render" alt="render">
